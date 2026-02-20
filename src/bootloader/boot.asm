f8ccb1f2e989c1502834c211dfb91fedc11534e3
Elias Meyer <eliasmeyer81908@gmail.com>
2026-02-20 10:55:43 -0800

    Added disk reading

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -64,14 +64,137 @@ main_start:
     mov ss, ax
     mov sp, 0x7C00
 
+    ;try to read data from disk
+    mov [ebr_drive_number], dl
+    mov ax, 1
+    mov cl, 1
+    mov bx, 0x7E00
+    call disk_read
+
     ;prints hello world
     mov si, helloword_msg
     call print_str
 
+    cli             ;disable interrupts
+    hlt
+
+floppy_error:
+    mov si, msg_read_failed
+    call print_str
+    jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h
+    jmp 0FFFFh:0    ;jmps to beginning of bios, should begin reboot
+
 .halt:
+    cli         ;disable interrupts so CPU can't get out of halt state
     jmp .halt
 
-helloword_msg: db 'Hello World!', ENDL, 0
+;converts lba to chs
+;takes lba in ax
+;return chs address in following format:
+; cx - [bits 0-5]: sector number
+; cx - [bits 6-15]: cylinder number
+; dx - dh: head number
+lba_to_chs:
+
+    push ax
+    push dx
+
+    xor dx, dx
+
+    ;makes ax = LBA / sectorsPerTrack
+    ;makes dx = LBA % sectorsPerTrack
+    div WORD [bdb_sectors_per_track]
+
+    inc dx                       ;dx = (LBA % sectorsPerTrack + 1) = sector number
+    mov cx, dx
+
+    xor dx, dx
+
+    ;makes ax = (LBA / sectorsPerTrack) / Heads
+    ;makes dx = (LBA / sectorsPerTrack) % Heads
+    div word [bdb_heads]
+
+    mov dh, dl
+    mov ch, al
+    shl ah, 6
+    or cl, ah
+
+    pop ax
+    mov dl, al
+    pop ax
+    ret
+
+
+;reads sectors from a disk
+; params:
+;   - ax: LBA address
+;   - cl: number of sectors to read (only up to 128 bytes)
+;   - dl: drive number
+;   - es:bx: memory address where to store read date
+disk_read:
+
+    ;save registers
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cs
+    call lba_to_chs
+    pop ax
+
+    mov ax, 02h
+    mov di, 3       ;retry counter
+
+.retry:
+    pusha   ;save all registers
+    stc     ;sets carry flag
+    int 13h
+    jnc .done
+    popa
+    
+    ;failed read
+    call disk_reset
+
+    dec di
+    test di, di
+    jnz .retry
+
+.fail:
+    jmp floppy_error
+    
+
+.done:
+    popa
+
+    ;restore registers
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+
+;resets disk controller
+; params:
+;   dl: drive number
+disk_reset:
+    pusha
+    xor ah, ah
+    stc
+    int 13h
+    jc floppy_error
+    popa
+    ret
+
+
+helloword_msg:      db 'Hello World!', ENDL, 0
+msg_read_failed:    db 'Failed to read from disk', ENDL, 0
 
 times 510-($-$$) db 0
 dw 0AA55h
